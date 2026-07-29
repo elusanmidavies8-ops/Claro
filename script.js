f5d4700c945cd764ca5975f6c30585d4322af923
Ayomide <elusanmidavies8@gmail.com>
2026-07-30 00:33:20 +0100

    I fix the button animation error

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ document.getElementById('start-button').addEventListener('click', function() {
         document.getElementById('onboarding').classList.add('active');
     }, 1000);
 });
-
+/* this is the event listener for the second onboarding screen 
+ and it is responsible for rotating the button animation */
 document.getElementById('start-button2').addEventListener('click', function() {
-    document.getElementById('btn-text').style.display = 'none';
-    document.getElementById('btn-spinner').style.display = 'block';
-
+    document.getElementById('btn-text2').style.display = 'none';
+    document.getElementById('btn-spinner2').style.display = 'block';
+// this is the timeout function that will remove the onboarding screen and add the next screen after 1 second
     setTimeout(function() {
         document.getElementById('onboarding').classList.remove('active');
         document.getElementById('onboarding-2').classList.add('active');
