71a958f542b0c194c0cf986dde5f5cba3a95e72f
Ayomide <elusanmidavies8@gmail.com>
2026-08-19 23:20:20 +0100

    I made little changes in the auth.css file and html file and i also tried to use localstorage to store users login data but I need to create the sign up page before that so i'll be sopping here tonight

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+/*const email = document.getElementById("email");
+const password = document.getElementById("password");
+const emailValue = email.value.trim();
+const passwordValue = password.value.trim();
+function userAuthenticate(){
+    const storedEmail = localStorage.getItem("userEmail" );
+    const storedPassword = localStorage.getItem("userPassword");
+    if (emailValue === storedEmail && passwordValue === storedPassword){
+        alert("Login successful");
+        window.location.href = "dashoard.html";
+    }else {
+        alert("invalid credentials");
+    }
+}
+document.getElementById("login-btn").addEventListner("click", userAuthenticate());
+*/
